a3f0dc4216c9c3d6930b9ffbe2019236660dad63
Luiz Girotto <luizfelipe.g@hotmail.com>
2016-08-10 18:45:37 -0300

    Added comments, indenting

--- scripts/CombatNode.gd
+++ scripts/CombatNode.gd
@@ -7,7 +7,11 @@ var char_database
 var window_size
 
 func _ready():
+	
+	# Acess character database (is a global script) #
 	char_database = get_node("/root/character_database")
+	
+	# Get window size #
 	window_size = OS.get_window_size()
 
 
@@ -17,33 +21,40 @@ func _ready():
 	instance_unit(0, "Enemies")
 	instance_unit(0, "Enemies")
 	instance_unit(0, "Enemies")
-	########################
+	# TESTING INSTANCING END #
 
 	reposition_units()
+	
 	resize_menu()
 
 
 func instance_unit(id, path):
+	
+	# Initialize visuals #
 	var anim_sprite = AnimatedSprite.new()
 	var anim_player = AnimationPlayer.new()
 
+	# Get folder and animation names for the character #
 	var char_folder = char_database.get_char_folder(id)
 	var anim_names = char_database.get_animation_array(id)
 
-
+	# Add animations to the player, play the idle animation #
 	for i in range(anim_names.size()):
 		anim_player.add_animation(anim_names[i], load(str(char_folder, anim_names[i], ".xml")))
 	anim_player.play("idle")
 
+	# Adjust sprite details
 	anim_sprite.set_sprite_frames(load(str(char_folder, char_database.get_char_name(id), ".tres")))
 	anim_sprite.set_scale(Vector2(scale, scale))
 
 	anim_sprite.add_child(anim_player)
 	get_node(path).add_child(anim_sprite)
-# Transferir as funções de extração de dados, para ficar mais #
-# claro para nós, ao fazer os scripts, o que estamos fazendo. #
 
 func reposition_units():
+	# This function might be altered later, so #
+	# enemy units will appear in different     #
+	# patterns, depending on their numbers.    #
+	
 	var num = 0
 	var temp = 1
 
@@ -60,8 +71,6 @@ func reposition_units():
 		child.set_pos(Vector2(window_size.x - 300 + 50*temp, temp*500/(num + 1)))
 		child.set_scale(Vector2(-scale, scale))
 		temp += 1 
-
-
 
 func resize_menu():
 	get_node("ActionMenu").set_size(Vector2(window_size.x, window_size.y - 500))
