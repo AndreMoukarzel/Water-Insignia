3b0e06efd16f49a198d6c66e03063686bcb526a8
Detril <andremoukarzel@hotmail.com>
2016-08-10 10:50:05 -0300

    Adds bat and interactive buttons

--- scripts/CombatNode.gd
+++ scripts/CombatNode.gd
@@ -8,10 +8,39 @@ func _ready():
 
 	resize_menu()
 
+
 func resize_menu():
 	get_node("ActionMenu").set_size(Vector2(window_size.x, window_size.y - 500))
 	get_node("ActionMenu/Selection").set_size(Vector2(window_size.x, window_size.y - 500))
 	get_node("ActionMenu/Attack").set_size(Vector2(window_size.x, window_size.y - 500))
 	get_node("ActionMenu/Skill").set_size(Vector2(window_size.x, window_size.y - 500))
 	get_node("ActionMenu/Item").set_size(Vector2(window_size.x, window_size.y - 500))
-	get_node("ActionMenu/Return").set_pos(Vector2(window_size.x - 50, window_size.y - 550))
+	get_node("ActionMenu/Return").set_pos(Vector2(window_size.x - 50, -50))
+
+
+func _on_Return_pressed():
+	var action_menu = get_node("ActionMenu")
+
+	action_menu.get_node("Selection").show()
+	action_menu.get_node("Attack").hide()
+	action_menu.get_node("Skill").hide()
+	action_menu.get_node("Item").hide()
+	action_menu.get_node("Return").hide()
+
+
+func _on_Attack_pressed():
+	get_node("ActionMenu/Selection").hide()
+	get_node("ActionMenu/Return").show()
+	get_node("ActionMenu/Attack").show()
+
+
+func _on_Skill_pressed():
+	get_node("ActionMenu/Selection").hide()
+	get_node("ActionMenu/Return").show()
+	get_node("ActionMenu/Skill").show()
+
+
+func _on_Item_pressed():
+	get_node("ActionMenu/Selection").hide()
+	get_node("ActionMenu/Return").show()
+	get_node("ActionMenu/Item").show()
--- scripts/mainmenu.gd
+++ scripts/mainmenu.gd
@@ -8,6 +8,10 @@ func _ready():
 	screen_size = OS.get_screen_size()
 	window_size = OS.get_window_size()
 
+	size_update()
+
+
+func size_update():
 	OS.set_window_resizable(0)
 	set_size(window_size)
 	get_node("Base").set_size(window_size)
@@ -23,8 +27,18 @@ func _on_Continue_pressed():
 
 
 func _on_Settings_pressed():
-	pass # replace with function body
+	get_node("Base").hide()
+	get_node("SettingsMenu").show()
 
 
 func _on_Exit_pressed():
 	get_tree().quit()
+
+
+func _on_WindowSize_item_selected( ID ):
+	pass # replace with function body
+
+
+func _on_Back_pressed():
+	get_node("Base").show()
+	get_node("SettingsMenu").hide()
