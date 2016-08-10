1e276b1d16a6ad8b4e556a452963abbeb268d38d
Detril <andremoukarzel@hotmail.com>
2016-08-10 17:44:23 -0300

    Develops database and character instancing from script

--- scripts/CombatNode.gd
+++ scripts/CombatNode.gd
@@ -1,7 +1,8 @@
 
 extends Node2D
 
-var char_database = preload("character_database.gd")
+const char_database = preload("character_database.gd")
+
 var window_size
 
 func _ready():
@@ -9,17 +10,19 @@ func _ready():
 
 	var bat_scn = preload("res://characters/monsters/bat/bat.xscn")
 
+
  # TESTING CREATING CHAR FROM SCRIPT #
-	var anim_sprite = AnimatedSprite.new()
-	var anim_player = AnimationPlayer.new()
-	
-	anim_player.add_animation("idle",load("res://characters/monsters/bat/idle.xml"))
-	anim_player.play("idle")
-	anim_sprite.set_pos(Vector2(200, 200))
-	anim_sprite.set_sprite_frames(load("res://characters/monsters/bat/bat.tres"))
-	anim_sprite.add_child(anim_player)
-	anim_sprite.set_scale(Vector2(5,5))
-	add_child(anim_sprite)
+#	var anim_sprite = AnimatedSprite.new()
+#	var anim_player = AnimationPlayer.new()
+#	
+#	anim_player.add_animation("idle",load("res://characters/monsters/bat/idle.xml"))
+#	anim_player.play("idle")
+#	anim_sprite.set_pos(Vector2(200, 200))
+#	anim_sprite.set_sprite_frames(load("res://characters/monsters/bat/bat.tres"))
+#	anim_sprite.add_child(anim_player)
+#	anim_sprite.set_scale(Vector2(5,5))
+#	add_child(anim_sprite)
+	instance_unit(0)
 
 ########################################
 	# TESTING INSTANCING #
@@ -40,8 +43,19 @@ func instance_unit(id):
 	var anim_sprite = AnimatedSprite.new()
 	var anim_player = AnimationPlayer.new()
 
-#	anim_player.add_animation(char_database.char_database[0]
+	var char_folder = char_database.get_char_folder(id)
+	var anim_names = char_database.get_animation_array(id)
 
+	for i in range(anim_names.size()):
+		anim_player.add_animation(anim_names[i], load(str(char_folder, anim_names, ".xml")))
+	anim_player.play("idle")
+
+	anim_sprite.set_pos(Vector2(200, 200))
+	anim_sprite.set_sprite_frames(load(str(char_folder, char_database.get_char_name(id), ".tres")))
+	anim_sprite.set_scale(Vector2(5,5))
+
+	anim_sprite.add_child(anim_player)
+	add_child(anim_sprite)
 # Transferir as funções de extração de dados, para ficar mais #
 # claro para nós, ao fazer os scripts, o que estamos fazendo. #
 
--- scripts/character_database.gd
+++ scripts/character_database.gd
@@ -19,7 +19,7 @@ const DEF = 5
 
 var char_database = [
 	{
-		FOLDER : "res://characters/monsters/bat",
+		FOLDER : "res://characters/monsters/bat/",
 		CHAR_NAME : "Bat",
 		ANIM_ARRAY : ["idle", "attack"],
 		HP_MAX : 10,
@@ -28,18 +28,24 @@ var char_database = [
 	}
 ]
 
-# new #
+
 var cd_map = { }
 
 func _ready():
-	pass
+	for id in range (char_database.size()):
+		cd_map[char_database[id][CHAR_NAME]] = id
 
+func get_char_id(name):
+	return cd_map[name]
 
 func get_char_folder(id):
 	return char_database[id][FOLDER]
 
 func get_char_name(id):
 	return char_database[id][CHAR_NAME]
+
+func get_animation_array(id):
+	return char_database[id][ANIM_ARRAY]
 
 # WIP #
 func get_animation_name(id, anim_num):
