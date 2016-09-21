b46d0a4b409f8bc23009c4998f4658af71190f3e
Luiz Girotto <luizfelipe.g@hotmail.com>
2016-09-21 08:50:27 -0300

    Added functionality for "change' button, fixed sprite bugs

--- scripts/ManagementNode.gd
+++ scripts/ManagementNode.gd
@@ -3,7 +3,10 @@ extends Node
 
 const SLOT_SIZE = 64
 
-var last_selected = -1
+var last_selected_party = -1
+var last_selected_barracks = -1
+
+var item_swap_mode = 0 # 0 = barracks, 1 = storage
 
 var char_database
 var wpn_database
@@ -17,6 +20,9 @@ var barracks_units = []
 
 var active_unit_weapons = []
 var barracks_unit_weapons = []
+
+var storage_weapons = []
+var storage_items = []
 
 var current_screen 
 
@@ -111,15 +117,24 @@ func _fixed_process(delta):
 		
 	# Item management
 	if (get_node("ItemManagement/ActiveParty").get_selected_items().size() != 0):
-		#populate and show weapons list
-		if (last_selected != get_node("ItemManagement/ActiveParty").get_selected_items()[0]):
+		#populate and show weapons list for active party member
+		if (last_selected_party != get_node("ItemManagement/ActiveParty").get_selected_items()[0]):
 			get_node("ItemManagement/ActivePartyWeapons").clear()
 			for weapon in active_units[get_node("ItemManagement/ActiveParty").get_selected_items()[0]].wpn_vector:
 				get_node("ItemManagement/ActivePartyWeapons").add_item("", load(str("res://resources/sprites/weapons/",weapon.name,".tex")), 1)
 				get_node("ItemManagement/ActivePartyWeapons").set_item_tooltip(get_node("ItemManagement/ActivePartyWeapons").get_item_count() - 1, weapon.name)
-			last_selected = get_node("ItemManagement/ActiveParty").get_selected_items()[0]
+			last_selected_party = get_node("ItemManagement/ActiveParty").get_selected_items()[0]
+	# Vai precisar da condição do mode tambem (ser o modo de trocar com os membros das barracks, não com o storage)
+	if (get_node("ItemManagement/Barracks").get_selected_items().size() != 0):
+		#populate and show weapons list for active party member
+		if (last_selected_barracks != get_node("ItemManagement/Barracks").get_selected_items()[0]):
+			get_node("ItemManagement/BarracksWeapons").clear()
+			for weapon in barracks_units[get_node("ItemManagement/Barracks").get_selected_items()[0]].wpn_vector:
+				get_node("ItemManagement/BarracksWeapons").add_item("", load(str("res://resources/sprites/weapons/",weapon.name,".tex")), 1)
+				get_node("ItemManagement/BarracksWeapons").set_item_tooltip(get_node("ItemManagement/ActivePartyWeapons").get_item_count() - 1, weapon.name)
+			last_selected_barracks = get_node("ItemManagement/Barracks").get_selected_items()[0]
 	
-	if (get_node("ItemManagement/ActivePartyWeapons").get_selected_items().size() != 0 or get_node("ItemManagement/Barracks").get_selected_items().size() != 0):
+	if (get_node("ItemManagement/ActivePartyWeapons").get_selected_items().size() != 0 or get_node("ItemManagement/BarracksWeapons").get_selected_items().size() != 0):
 		get_node("ItemManagement/SwapItems").set_disabled(false)
 	else:
 		get_node("ItemManagement/SwapItems").set_disabled(true)
@@ -146,6 +161,11 @@ func size_update():
 	get_node("ItemManagement/BarracksWeapons").set_pos(Vector2(get_node("ItemManagement/Barracks").get_pos().x, 300))
 	get_node("ItemManagement/BarracksItems").set_size(Vector2(300, 70))
 	get_node("ItemManagement/BarracksItems").set_pos(Vector2(get_node("ItemManagement/Barracks").get_pos().x, 400))
+	
+	get_node("ItemManagement/StorageWeapons").set_size(Vector2(window_size.x/3, window_size.y/3))
+	get_node("ItemManagement/StorageWeapons").set_pos((Vector2(window_size.x - 40 - get_node("ItemManagement/StorageWeapons").get_size().x, 40)))
+	get_node("ItemManagement/StorageItems").set_size(Vector2(window_size.x/3, window_size.y/3))
+	get_node("ItemManagement/StorageItems").set_pos((Vector2(get_node("ItemManagement/Barracks").get_pos().x, get_node("ItemManagement/Barracks").get_pos().y + 220)))
 # #################################### #
 # ##### UNIT MANAGEMENT FUNCTIONS #### # 
 # #################################### #
@@ -190,7 +210,26 @@ func _on_Swap_pressed():
 		barracks_units.remove(b_local_id)
 		get_node("UnitManagement/Barracks").remove_item(b_local_id)
 		get_node("ItemManagement/Barracks").remove_item(b_local_id)
-
+		
+func _on_SwapItems_pressed():
+	pass # replace with function body
+	# Only active party item selected
+	
+func _on_StorageBarracks_pressed():
+	if (item_swap_mode == 0):
+		get_node("ItemManagement/Barracks").hide()
+		get_node("ItemManagement/BarracksWeapons").hide()
+		get_node("ItemManagement/BarracksItems").hide()
+		get_node("ItemManagement/StorageWeapons").show()
+		get_node("ItemManagement/StorageItems").show()
+		item_swap_mode = 1
+	else:
+		get_node("ItemManagement/Barracks").show()
+		get_node("ItemManagement/BarracksWeapons").show()
+		get_node("ItemManagement/BarracksItems").show()
+		get_node("ItemManagement/StorageWeapons").hide()
+		get_node("ItemManagement/StorageItems").hide()
+		item_swap_mode = 0
 
 # ################################ #
 # ###### MENU FUNCTIONALITY ###### # 
@@ -238,10 +277,6 @@ func _on_ManageItems_pressed():
 	get_node("ItemManagement").show()
 
 	current_screen = "ItemManagement"
-
-
-func _on_SwapItems_pressed():
-	pass # replace with function body
 	
 # Funções temporarias de teste
 func instance_unit(id, path):
